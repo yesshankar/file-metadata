8b9ac3e090113da0a7d51508824d9cb82c578daf
Shankar Khatri <yes143.shankar@gmail.com>
2019-05-07 18:25:51 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ... at port 3000');
+});
